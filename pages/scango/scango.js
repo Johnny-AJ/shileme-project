d48f2bf1671766fddd018d586d2f2eb1563707a6
llc4399 <LLC4399@163.com>
2019-11-27 20:14:12 +0800

    第一次合并

---
+++ pages/scango/scango.js
@@ -0,0 +1,95 @@
+// pages/scango/scango.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    currentSwiper: 0,
+    autoplay: true,
+    showDialog: false,
+    showDialog1: false,
+    boolean:true
+  },
+  toggleDialog() {
+    
+    this.setData({
+      showDialog: !this.data.showDialog
+    });
+  },
+  toggleDialog1() {
+  
+    this.setData({
+      showDialog1: !this.data.showDialog1
+    });
+  },
+  swiperChange: function(e) {
+    this.setData({
+      currentSwiper: e.detail.current
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+    wx.setNavigationBarTitle({
+      title: '商品详情'
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
