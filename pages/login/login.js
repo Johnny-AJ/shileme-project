5ff0f7b3526c57c665abcac5358f80b297e722dc
高桥老湿 <gaoqiaolaoshi@qq.com>
2019-12-02 20:02:46 +0800

    上传1.04

--- pages/login/login.js
+++ pages/login/login.js
@@ -6,7 +6,41 @@ Page({
    */
   data: {
 
+    // 存储用户信息
+    login: function() {
+      if (wx.setStorageSync('key', 'token')) return;
+      var that = this
+      wx.login({
+        sunccess: function(res) {
+          if (res.code) {
+            wx.request({
+              url: 'http://192.168.2.119:9095/api/wechat/auth',
+              data: {
+                code: res.code
+              },
+              method: 'GET',
+              success: function(res) {
+                if (res.status == 'success' && res.detail.userInfo) {
+                  wx.setStorageSync("token", res.detail.userInfo);
+                  wx.redirectTo({
+                    url: "pages/index/index",
+                  })
+                }
+              }
+            })
+          }
+        },
+        fail: function(res) {
+          console.log("网络错误");
+        },
+        complete: function(res) {
+
+        }
+      })
+    }
+
   },
+  // 用户信息
   handgetuserInfo(e) {
     // console.log(e)
   },
@@ -16,53 +50,4 @@ Page({
   onLoad: function(options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  }
 })
