563a293fee1013244633c120220183dee06c4ed6
llc4399 <LLC4399@163.com>
2019-12-10 11:42:54 +0800

    版本1.13

--- pages/Spell_group_order/Spell_group_order.js
+++ pages/Spell_group_order/Spell_group_order.js
@@ -1,4 +1,3 @@
-
 Page({
 
   /**
@@ -10,8 +9,14 @@ Page({
     propertyId: '344',
     num: '2',
     token: '',
-    dtos:[]
-    
+    dtos: [],
+    address: {},
+    addressok: false,
+    orderWaresVos: {},
+    quota:{},
+    allprice:0,
+    inputValue:''
+
   },
 
   /**
@@ -20,53 +25,20 @@ Page({
 
 
   onLoad: function(options) {
-
-     wx.setNavigationBarTitle({
-      title: '下单',
-    })
-
-    // 获取扫码购的参数
-    var list = JSON.parse(options.dtos)
-  console.log(5656,list)
-    let dd=[];
-    var dtos = dd.concat(list)
-    this.setData({
-      dtos: dtos
-    })
-    var self = this;
-
-    const token = wx.getStorageSync('token')
-    self.setData({
-      token
-    })
-    console.log(88888, dtos)
-    wx.request({
-      url: 'http://192.168.2.119:9095/api/order/savePlace',
-      method: 'POST',
-      data: {
-        dtos: dtos
-      },
-      header: {
-        token: self.data.token
-      },
-      success: (res) => {
-        console.log(6666,res)
-      },
-      fail: (error) => {
-        console.log(777,error)
-      }
-
-    })
-
    
+   
+    this.oders(options); // 获取扫码购的参数
+    var self = this;
+    if (self.data.address) {
+
+    } else {
+      self.setData({
+        addressok: true
+      })
+    }
 
   },
-  toggleDialog() {
-    this.setData({
-      showDialog: !this.data.showDialog,
-
-    });
-  },
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -102,25 +74,28 @@ Page({
   onPullDownRefresh: function() {
 
   },
-  goto(e) {  //提交订单
+  goto(e) { //提交订单
     console.log(this.data.dtos)
-   
+
 
     wx.request({
       // 支付地址
-      url: 'http://192.168.2.119:9095/api/order/saveOrder',
+      url: 'http://192.168.2.95:9095/api/order/saveOrder',
       method: "POST",
       header: {
         'token': this.data.token, //请求头携带参数
       },
-      data:{
-        addressId:1,
-        waresList: this.data.dtos
+      data: {
+        discountId:'',
+        remark: this.data.inputValue,
+        addressId: 1,
+        waresList: this.data.dtos,
+
       },
       success: function(res) {
-        console.log(78888,res)
+        // console.log(78888,res)
 
-    
+
         // 微信支付
         wx.requestPayment({
           timeStamp: res.data.data.timeStamp,
@@ -133,8 +108,8 @@ Page({
             wx.navigateTo({
               url: e.currentTarget.dataset.url,
             })
-           },
-          fail(res) { 
+          },
+          fail(res) {
             wx.navigateTo({
               url: e.currentTarget.dataset.url,
             })
@@ -157,5 +132,80 @@ Page({
    */
   onShareAppMessage: function() {
 
-  }
+  },
+  oders(options) {//获取下单详细信息
+
+    var self = this;
+    var list = JSON.parse(options.dtos)
+
+    let dd = [];
+    var dtos = dd.concat(list)
+    self.setData({
+      dtos: dtos
+    })
+    const token = wx.getStorageSync('token')
+    self.setData({
+      token
+    })
+    // console.log(88888, dtos)
+    wx.request({
+      url: 'http://192.168.2.98:9095/api/order/savePlace',
+      method: 'POST',
+      data: {
+        dtos: dtos
+      },
+      header: {
+        token: self.data.token
+      },
+      success: (res) => {
+        console.log(6666, res.data)
+
+        self.setData({
+          allprice: res.data.data.allprice
+        })
+
+        self.setData({
+          address: res.data.data.address
+        })
+        
+        self.setData({
+          orderWaresVos: res.data.data.orderWaresVos
+        })
+
+
+
+
+
+      },
+      fail: (error) => {
+        // console.log(777,error)
+      }
+
+    })
+
+
+
+  },
+
+  handurl: function(e) {//路由跳转到填写地址
+    console.log(e)
+    // 路由封装
+    wx.navigateTo({
+      url: '/pages/shipping/shipping',
+    })
+  },
+  toggleDialog() {//弹出页关闭
+    this.setData({
+      showDialog: !this.data.showDialog,
+
+    });
+  },
+
+  bindKeyInput: function (e) {//卖家留言
+    this.setData({
+      inputValue: e.detail.value
+    })
+
+    console.log(this.data.inputValue)
+  },
 })
