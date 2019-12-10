e70a2bbd7d38efd0e2ca7a35378ae062a4d543fe
llc4399 <LLC4399@163.com>
2019-12-10 11:11:50 +0800

    版本1.12

--- pages/scango/scango.js
+++ pages/scango/scango.js
@@ -1,4 +1,3 @@
-// pages/scango/scango.js
 Page({
 
   /**
@@ -6,24 +5,130 @@ Page({
    */
   data: {
     value1: 1,
-    token: '',
-    contents: {},
-    imgs: [],
-    value1: 1,
-    commodityAttr: [],
-    imgindex: 0,
+    list:{},//初始化数据
+    imgUrls: [],//轮播图片
+    currentSwiper: 0,//滑块下标
+    showDialog: false,//领劵开关
+    showDialog1: false,//商品规格弹出开关
+    boolean: true,//关闭弹出层开关
+    commodityAttr:[],
     attrValueList: [],
-    imgUrls: [],
-    currentSwiper: 0,
-    autoplay: true,
-    showDialog: false,
-    showDialog1: false,
-    boolean: true,
-    list: {},
-    comments: {}
+    imgsurl:[],
+    waresId:''
   },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      waresId: options.waresId
+    })
+
+    // console.log(waresId,'waresId')
+    this.get_data(options.waresId); // 获取商品数据
+    this.selection(options.waresId);//商品规格选择
+   
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+
+
+ 
+  // 获取商品数据
+  get_data(waresId){
+    var self=this;
+    wx.request({
+      url: 'http://192.168.2.98:9095/api/wares/details/getWaresInfo',
+      method: "get",
+      data: {
+        waresId: 9//waresId
+      },
+      success(res) {
+        console.log(66666, res)
+        self.setData({
+          list: res.data.data
+        })
+
+
+      }
+    })
+  },
+  selection(waresId) {   // 规格选择
+    var self=this;
+    wx.request({
+      url: 'http://192.168.2.98:9095/api/wares/details/getPropertyList',
+      method: "get",
+      data: {
+        waresId:9//waresId
+      },
+      success(res) {
+       
+        self.setData({
+          imgsurl: res.data.data.imgs
+        });
+
+        console.log(1111222, self.data)
+        self.setData({
+          commodityAttr: res.data.data.commodityAttr
+        });
+
+        self.setData({
+          includeGroup: self.data.commodityAttr
+        });
+        
+
+        self.distachAttrValue(self.data.commodityAttr);
+        // 只有一个属性组合的时候默认选中 
+        // console.log(this.data.attrValueList); 
+        if (self.data.commodityAttr.length == 1) {
+          for (var i = 0; i < self.data.commodityAttr[0].attrValueList.length; i++) {
+            self.data.attrValueList[i].selectedValue = self.data.commodityAttr[0].attrValueList[i].attrValue;
+          }
+          self.setData({
+            attrValueList: self.data.attrValueList
+          });
+
+        
+        }
+      }
+    })
+  }
+  /**
+   * 用户点击右上角分享
+   */
+  ,
+ 
   // 弹出领劵层方法
   toggleDialog() {
+  
     this.setData({
       showDialog: !this.data.showDialog,
 
@@ -36,13 +141,13 @@ Page({
     });
 
   },
-  swiperChange: function(e) {
+  // 轮播图
+  swiperChange: function (e) {
     this.setData({
       currentSwiper: e.detail.current
     })
   },
   // input输入框
-
   handleChange1({
     detail
   }) {
@@ -50,70 +155,8 @@ Page({
       value1: detail.value
     })
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
-    // 头部标题
-    wx.setNavigationBarTitle({
-      title: '扫码购'
-    }),
-      this.request()
-  },
-  /**
-   * 生命周期函数--监听页面显示
-   */
-
-  
-  onShow: function() {
-
-  var self=this;
-    // 评论请求
-    // wx.request({
-    //   url: 'http://192.168.2.98:9095/api/wares/details/getCommentList',
-    //   data: {
-    //     start: 0,
-    //     limit: 4,
-    //     waresId: 28
-    //   },
-    //   success(res) {
-    //     comments: res.data
-    //   }
-    // })
-    // 规格选择
-    wx.request({
-        url: 'http://192.168.2.119:9095//api/wares/details/getPropertyList',
-        method: "get",
-        data: {
-          waresId: 28
-        },
-        success(res) {
-          console.log(1111, res.data.data)
-          self.setData({
-            commodityAttr: res.data.data.commodityAttr
-          })
-        }
-      }),
-      this.setData({
-        includeGroup: this.data.commodityAttr
-      });
-    this.distachAttrValue(this.data.commodityAttr);
-    // 只有一个属性组合的时候默认选中 
-    // console.log(this.data.attrValueList); 
-    if (this.data.commodityAttr.length == 1) {
-      for (var i = 0; i < this.data.commodityAttr[0].attrValueList.length; i++) {
-        this.data.attrValueList[i].selectedValue = this.data.commodityAttr[0].attrValueList[i].attrValue;
-      }
-      this.setData({
-        attrValueList: this.data.attrValueList
-      });
-    }
-
-  },
-  /* 获取数据 */
-  distachAttrValue: function(commodityAttr) {
-    var self = this;
-
+  //规格选择
+  distachAttrValue: function (commodityAttr) {
     /** 
     将后台返回的数据组合成类似 
     { 
@@ -157,7 +200,7 @@ Page({
       attrValueList: attrValueList
     });
   },
-  getAttrIndex: function(attrName, attrValueList) {
+  getAttrIndex: function (attrName, attrValueList) {
     // 判断数组中的attrKey是否有该属性值 
     for (var i = 0; i < attrValueList.length; i++) {
       if (attrName == attrValueList[i].attrKey) {
@@ -166,7 +209,7 @@ Page({
     }
     return i < attrValueList.length ? i : -1;
   },
-  isValueExist: function(value, valueArr) {
+  isValueExist: function (value, valueArr) {
     // 判断是否已有属性值 
     for (var i = 0; i < valueArr.length; i++) {
       if (valueArr[i] == value) {
@@ -176,7 +219,7 @@ Page({
     return i < valueArr.length;
   },
   /* 选择属性值事件 */
-  selectAttrValue: function(e) {
+  selectAttrValue: function (e) {
     /* 
     点选属性值，联动判断其他属性值是否可选 
     { 
@@ -188,7 +231,7 @@ Page({
     console.log(e.currentTarget.dataset); 
     */
     var attrValueList = this.data.attrValueList;
-    var index = e.currentTarget.dataset.index; //属性索引 
+    var index = e.currentTarget.dataset.index;//属性索引 
     var key = e.currentTarget.dataset.key;
     var value = e.currentTarget.dataset.value;
     if (e.currentTarget.dataset.status || index == this.data.firstIndex) {
@@ -199,11 +242,14 @@ Page({
         // 选中 
         this.selectValue(attrValueList, index, key, value);
       }
+
     }
   },
   /* 选中 */
-  selectValue: function(attrValueList, index, key, value, unselectStatus) {
-    // console.log('firstIndex', this.data.firstIndex); 
+  selectValue: function (attrValueList, index, key, value, unselectStatus) {
+
+
+    console.log('firstIndex', this.data.firstIndex); 
     var includeGroup = [];
     if (index == this.data.firstIndex && !unselectStatus) { // 如果是第一个选中的属性值，则该属性所有值可选 
       var commodityAttr = this.data.commodityAttr;
@@ -217,6 +263,7 @@ Page({
     } else {
       var commodityAttr = this.data.includeGroup;
     }
+
     // console.log('选中', commodityAttr, index, key, value); 
     for (var i = 0; i < commodityAttr.length; i++) {
       for (var j = 0; j < commodityAttr[i].attrValueList.length; j++) {
@@ -226,6 +273,7 @@ Page({
       }
     }
     attrValueList[index].selectedValue = value;
+
     // 判断属性是否可选 
     for (var i = 0; i < attrValueList.length; i++) {
       for (var j = 0; j < attrValueList[i].attrValues.length; j++) {
@@ -245,11 +293,12 @@ Page({
         }
       }
     }
-    // console.log('结果', attrValueList); 
+    console.log('结果', attrValueList); 
     this.setData({
       attrValueList: attrValueList,
       includeGroup: includeGroup
     });
+
     var count = 0;
     for (var i = 0; i < attrValueList.length; i++) {
       for (var j = 0; j < attrValueList[i].attrValues.length; j++) {
@@ -259,7 +308,7 @@ Page({
         }
       }
     }
-    if (count < 2) { // 第一次选中，同属性的值都可选 
+    if (count < 2) {// 第一次选中，同属性的值都可选 
       this.setData({
         firstIndex: index
       });
@@ -270,9 +319,10 @@ Page({
     }
   },
   /* 取消选中 */
-  disSelectValue: function(attrValueList, index, key, value) {
+  disSelectValue: function (attrValueList, index, key, value) {
     var commodityAttr = this.data.commodityAttr;
     attrValueList[index].selectedValue = '';
+
     // 判断属性是否可选 
     for (var i = 0; i < attrValueList.length; i++) {
       for (var j = 0; j < attrValueList[i].attrValues.length; j++) {
@@ -283,52 +333,15 @@ Page({
       includeGroup: commodityAttr,
       attrValueList: attrValueList
     });
+
     for (var i = 0; i < attrValueList.length; i++) {
       if (attrValueList[i].selectedValue) {
         this.selectValue(attrValueList, i, attrValueList[i].attrKey, attrValueList[i].selectedValue, true);
       }
     }
   },
-  submit: function() {
-    var value = [];
-    for (var i = 0; i < this.data.attrValueList.length; i++) {
-      if (!this.data.attrValueList[i].selectedValue) {
-        break;
-      }
-      value.push(this.data.attrValueList[i].selectedValue);
-    }
-    if (i < this.data.attrValueList.length) {
-      wx.showToast({
-        title: '请选择完整！',
-        icon: 'loading',
-        duration: 1000
-      })
-    } else {
-      var valueStr = "";
-      for (var i = 0; i < value.length; i++) {
-        console.log(value[i]);
-        valueStr += value[i] + ",";
-      }
-      wx.showModal({
-        title: '提示',
-        content: valueStr,
-        success: function(res) {
-          if (res.confirm) {
-            console.log('用户点击确定')
-          } else if (res.cancel) {
-            console.log('用户点击取消')
-          }
-        }
-      })
-      console.log(valueStr);
-    }
-  },
-  goto: function(e) {
-    wx.navigateTo({
-      url: '/pages/comment/comment',
-    })
-  },
-  buys: function(e) {
+  // 立即购买
+  buys: function (e) {
 
     console.log(e)
     var dtos = JSON.stringify(e.currentTarget.dataset);
@@ -337,29 +350,5 @@ Page({
     wx.navigateTo({
       url: '/pages/Spell_group_order/Spell_group_order?dtos=' + dtos
     })
-  },
-  request() {
-   
-    var self = this;
-    wx.setNavigationBarTitle({
-        title: '商品详情'
-      }),
-      // 数据请求
-      wx.request({
-        url: 'http://192.168.2.98:9095/api/wares/details/getWaresInfo',
-        method: "get",
-        data: {
-          waresId: 28
-        },
-        success(res) {
-          console.log(66666,res)
-          self.setData({
-            list: res.data.data
-          })
-
-
-        }
-      })
   }
-
 })
