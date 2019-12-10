82e70590ce4eea4854df1c32de14e69f1bd27f26
高桥老湿 <gaoqiaolaoshi@qq.com>
2019-12-10 11:19:45 +0800

    合并版本1.13

--- pages/formats/formats.js
+++ pages/formats/formats.js
@@ -10,8 +10,6 @@ Page({
       "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1575541027970&di=ecf2c0391eb72e50a428c14e99c39bf3&imgtype=0&src=http%3A%2F%2Fk.zol-img.com.cn%2Fdiybbs%2F5812%2Fa5811338_s.jpg",
       "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1575556772324&di=da3d003085891be4d696ed4fed3b6727&imgtype=0&src=http%3A%2F%2Fpic31.nipic.com%2F20130708%2F3347542_143402189000_2.jpg",
       "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1575542168801&di=457f23a469df75e92b17f5204a71a8f3&imgtype=0&src=http%3A%2F%2Fp0.qhimgs4.com%2Ft01034ed8722375e889.jpg",
-      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1575556846323&di=b5b36223aac6373857f0418adef3c410&imgtype=0&src=http%3A%2F%2Fpic1.16pic.com%2F00%2F01%2F39%2F16pic_139059_b.jpg",
-      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1575556851149&di=021fb9719762984ed0191087b8cbc2a7&imgtype=0&src=http%3A%2F%2Fpic39.nipic.com%2F20140328%2F6608733_084513055000_2.jpg",
       "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1575556890196&di=534195e404a30d70df59d8630c1c877d&imgtype=0&src=http%3A%2F%2Fpic35.nipic.com%2F20131114%2F3347542_141746002353_2.jpg"
     ],
   },
