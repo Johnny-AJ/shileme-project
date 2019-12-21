1e5e1752982e5a567b89de4fb52fa6ec0439067f
高桥老湿 <gaoqiaolaoshi@qq.com>
2019-12-21 19:18:32 +0800

    版本1.38

--- pages/find/find.js
+++ pages/find/find.js
@@ -1,20 +1,26 @@
-// pages/find/find.js
+// pages/formats/formats.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 轮播图
+    swiperList: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
-    // 头部标题
-    wx.setNavigationBarTitle({
-      title: '分类'
+  onLoad(e) {
+    // 轮播图
+    this.setSwiperData()
+  },
+  // 轮播图
+  setSwiperData() {
+    wx.request({
+      url: 'http://192.168.2.98:9095/api/index/banner/banners',
+      success: (res) => {
+        this.setData({
+          swiperList: res.data.data
+        })
+      }
     })
-  },
+  }
 })
